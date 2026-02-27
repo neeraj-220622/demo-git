d989a87c34042f6eee1eb4f7ddf3e4c267b14cf2
neeraj-220622 <n220622@rguktn.ac.in>
2026-02-27 20:59:50 +0530

    The initial project

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    alert("Feature is working perfectly!");
+}
+
+function enableNewFeature() {
+    document.getElementById("featureStatus").innerText =
+        "New Feature Enabled Successfully!";
+}
+
