f7680ad1f0f228f7d980f10f8eeaef901680a5ce
neeraj-220622 <n220622@rguktn.ac.in>
2026-02-27 21:20:25 +0530

    This is saved locally

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function enableNewFeature() {
         "New Feature Enabled Successfully!";
 }
 
+function loginUser() {
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "" || password === "") {
+        document.getElementById("loginMessage").innerText =
+            "Please enter both fields.";
+        return;
+    }
+
+    // INCOMPLETE AUTH LOGIC (Still Under Development)
+
+    if (username === "admin" && password === "1234") {
+        document.getElementById("loginMessage").innerText =
+            "Login successful! (Role system not implemented yet)";
+    } else {
+        document.getElementById("loginMessage").innerText =
+            "Invalid credentials (Database not connected yet)";
+    }
+}
